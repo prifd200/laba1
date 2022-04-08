6c3cb81d8c4eba86046b0916aa5b57abff1c0395
prifd200 <dank200kuz@mail.ru>
2022-04-08 22:31:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import time
 max_buffer_len = 100                                                                                                          # максимальный размер рабочего буфера
 try:
     with open("text.txt", "r", encoding="utf-8") as f:                                                                        # открываем файл
@@ -14,8 +15,9 @@ try:
                 sentences += len(re.findall(r'[.!?]+', buffer))                                                               # подсчитывает количество предложений
                 puncts += len(re.findall(r'[.!?:;",)\'(\-]', buffer))                                                         # подсчитывает количество знаков препинания
                 digits += len(re.findall(r'\d', buffer))                                                                      # подсчитывает количество цифр
-                numbers += len(re.findall(r'\b\d+\b', buffer))                                                                # подсчитывает количество чисел
+                numbers += len(re.findall(r'\b\d+[.,]?\d+\b', buffer))                                                                # подсчитывает количество чисел
                 buffer = f.read(max_buffer_len)                                                                               # читаем очередной блок
             print(f"В тексте:\n{lines} строк(и); {chars} символов; {words} слов; {sentences} предложений; {puncts} знаков препинания; {digits} цифр; {numbers} чисел")
 except FileNotFoundError:
     print("Файл text.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
+print("Время работы программы:  ", time.process_time(), "секунд")
